831a86acdf06cc317bcf7449266b429d2b5b66a1
Олександр <aleksandrlulenko@gmail.com>
2023-11-13 19:27:37 +0100

    ---

--- js/task-3.js
+++ js/task-3.js
@@ -1,4 +1,4 @@
-function getElementWidth(content, padding, border) { return (content + padding + padding + border + border) };
+function getElementWidth(content, padding, border) { return (content + padding + padding + padding + padding + border + border + border + border) };
 
 console.log(getElementWidth("50px", "8px", "4px"));
 console.log(getElementWidth("60px", "12px", "8.5px"));
