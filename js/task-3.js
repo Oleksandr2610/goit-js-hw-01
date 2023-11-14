f3d7715a34a1cffa936114b958b60ddd7bf66ea2
Олександр <aleksandrlulenko@gmail.com>
2023-11-14 15:39:07 +0100

    sss

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,4 @@
 function getElementWidth(content, padding, border) { return (content + padding + padding + padding + padding + border + border + border + border) };
-
 console.log(getElementWidth("50px", "8px", "4px"));
 console.log(getElementWidth("60px", "12px", "8.5px"));
 console.log(getElementWidth("200px", "0px", "0px"));
