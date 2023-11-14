474108ef9318330dcd7f5994b90f37449f04997c
Олександр <aleksandrlulenko@gmail.com>
2023-11-14 15:51:43 +0100

    ddd

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,5 @@
-function makeTransaction(country, price, deliveryFee) {
-    const totalPrice = price + deliveryFee;
+function getShippingMessage(country, price, deliveryFee) {
+    let totalPrice = price + deliveryFee;
     return (`Shipping to ${country} will cost ${totalPrice} credits`)
 };
 console.log(getShippingMessage("Australia", 120, 50));
